420823e75f17e09f0bf70f9cc161ad27a3c32918
rontsch <rontsch@db57ad2b-4091-4eb9-b09b-be0769b622ce>
2013-03-18 05:30:05 +0000

    routines for pol vector, for Z coupl and gamma coupl

--- mod_ZDecay.f90
+++ mod_ZDecay.f90
@@ -3,25 +3,38 @@ implicit none
 
 contains 
 
-  function ZGamPolVec(pl,pa,QType,LepType,QHel,LepHel)
+  function ZGamPolVec(pl,pa,LepHel)
 ! Polarization vector for Z/gamma decay to leptons
 ! pl, pa are momenta of lepton and antilepton
-! QType = (u,d,c,s,t,b), QHel is quark helicity
 ! LepType = 1 for charged leptons, 2 for neutrinos; 
 ! LepHel is lepton helicity
-! Can modify it to allow BSM ttZ couplings, F_V-F_A for LH top, F_V+F_A for RH
     use ModParameters
     use ModMisc
     implicit none
     complex(8)  :: pl(4), pa(4)
-    integer     :: QType, LepType,QHel, LepHel
+    integer     :: LepHel
     complex(8)  :: ZGamPolVec(4)
-    complex(8)  :: pZ(4),leppol(4),aleppol(4),propZ,propG
-    real(8)     :: couplZQQ, couplGQQ,couplZLL,couplGLL
+    complex(8)  :: pZ(4),leppol(4),aleppol(4)
     
     pZ=pl+pa
-    propZ=1d0/(sc_(pZ,pZ)-m_Z**2+ci*Ga_ZExp*m_Z)
-    propG=1d0/sc_(pZ,pZ)
+    
+    call ubarSpi_Weyl(pl,LepHel,leppol)
+    call vSpi_Weyl(pa,-LepHel,aleppol)
+    ZGamPolVec(1:4)=vbqq_Weyl(4,leppol,aleppol)
+
+! NB: note factor 1/pZ^2 in here:
+    ZGamPolVec=ZGamPolVec/(sc_(pZ,pZ))
+
+
+  end function ZGamPolVec
+
+
+  subroutine ZGamQcoupl(QType,QHel,couplZQQ,couplGQQ)
+! QType = (u,d,c,s,t,b), QHel is quark helicity
+! Can modify it to allow BSM ttZ couplings, F_V-F_A for LH top, F_V+F_A for RH
+    use ModParameters
+    integer, intent(in)     :: QType, QHel
+    real(8), intent(out)     :: couplZQQ, couplGQQ
 
 ! Decide on quark charge and isospin    
     if ( mod(QType,2) .eq. 1) then      ! up-type quark
@@ -45,8 +58,21 @@ contains
     else
        stop "Error in ZDecay: QType undefined"
     endif
+
+  end subroutine ZGamQcoupl
     
-! Decide on lepton couplings
+
+  subroutine ZGamLCoupl(LepType,LepHel,couplZLL,couplGLL)
+! LepType = 1 for charged leptons, 2 for neutrinos; 
+! LepHel is lepton helicity
+    use ModParameters
+    use ModMisc
+    implicit none
+    integer, intent(in)     :: LepType,LepHel
+    real(8),intent(out)     :: couplZLL,couplGLL
+    
+
+ ! Decide on lepton couplings
     if (LepType .eq. 1) then        ! charged lepton
        if (LepHel .eq. -1) then
           couplZLL=couplZEE_left
@@ -68,14 +94,7 @@ contains
     else
        stop "Error in ZDecay: LepType undefined"
     endif
-    
-    call ubarSpi_Weyl(pl,LepHel,leppol)
-    call vSpi_Weyl(pa,-LepHel,aleppol)
-    ZGamPolVec(1:4)=vbqq_Weyl(4,aleppol,leppol)
 
-! now add couplings
-    ZGamPolVec=ZGamPolVec*(couplZQQ*couplZLL*propZ+couplGQQ*couplGLL*propG)
-
-  end function ZGamPolVec
+  end subroutine ZGamLCoupl
 
 end MODULE ModZDecay
