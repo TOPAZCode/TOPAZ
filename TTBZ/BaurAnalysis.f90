1b70d7d2d925e6fb2e667a072c8a35bde2799b78
rontsch <rontsch@db57ad2b-4091-4eb9-b09b-be0769b622ce>
2013-08-23 17:12:38 +0000

    rebinning added, output improved

--- TTBZ/BaurAnalysis.f90
+++ TTBZ/BaurAnalysis.f90
@@ -11,9 +11,12 @@ program BaurAnalysis
   implicit none
   integer,parameter :: MaxBins=200
   integer :: NumArgs,Histo,i,j,k,NumBins_SM,NumBins_BSM,NumBins,sum_Events_SM,NumEventBins
-  integer :: NHisto(1:MaxBins),Hits(1:MaxBins),Events_SM(1:MaxBins),Events_BSM(1:MaxBins)
+  integer :: NHisto(1:MaxBins),Hits(1:MaxBins),Events_SM(1:MaxBins),Events_BSM(1:MaxBins),rebin_exp
   real(8) :: PreFac,data,DeltaN,BinSize_SM,BinSize_BSM,BinSize,fbar_sq,f,chisq,P
   real(8) :: BinVal(1:MaxBins),Valu_SM(1:MaxBins),Error_SM(1:MaxBins),Valu_BSM(1:MaxBins),Error_BSM(1:MaxBins)
+  character :: DelF1V*(10), DelF1A*(10),cachestr*(10)
+  integer :: cache
+real(8) :: Valu_SM_rebin(1:MaxBins),Error_SM_rebin(1:MaxBins),Valu_BSM_rebin(1:MaxBins),Error_BSM_rebin(1:MaxBins)
   character ::  operation*(10), SM_infile*(50), BSM_infile*(50),dummy*(1),Histo_str*(5),data_str*(5),DeltaN_str*(5)
 character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.16,A,2X,I9,A)"
 
@@ -35,7 +38,7 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
      stop
   endif
 
-  if (NumArgs .ne. 6)   then
+  if (NumArgs .lt. 6)   then
      print *, 'Requires six arguments, you entered ', NumArgs
      stop
   endif
@@ -45,10 +48,22 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
   call GetArg(4,Histo_str)
   call GetArg(5,data_str)
   call GetArg(6,DeltaN_str)
+  call GetArg(7,Delf1V)     ! these are just for output, not needed??
+  call GetArg(8,Delf1A)
+  call GetArg(9,cachestr)
   read(Histo_str,"(I2)") Histo
   read(data_str,"(F10.6)") data
   read(DeltaN_str,"(F10.6)") DeltaN
-  
+  read(cachestr, "(I3)") cache
+! RR remove
+  data=10000d0
+  Delf1V=trim(Delf1V)
+  Delf1A=trim(Delf1A)
+
+  print *, 'Baur analysis with histogram #', Histo
+  print *, 'Integrated luminosity :', data
+  print *, 'Theoretical uncertainty :', deltaN 
+  print *, cache
   PreFac=PreFac*data
 
   open(unit=11,file=trim(SM_infile),form='formatted',access='sequential')
@@ -107,7 +122,7 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
    
    do i = 1,NumBins
       if (Valu_SM(i) .ge. 5d0 .and. Valu_BSM(i) .ge. 5d0) then
-!      if (Valu_SM(i) .ge. 1d0 .and. Valu_BSM(i) .ge. 1d0) then
+         !      if (Valu_SM(i) .ge. 1d0 .and. Valu_BSM(i) .ge. 1d0) then
          j=j+1
          Events_SM(j) =floor(Valu_SM(i))
          Events_BSM(j)=floor(Valu_BSM(i))
@@ -115,6 +130,29 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
          fbar_sq = fbar_sq + 1.d0*Events_BSM(j)**2/Events_SM(j)
       endif
    enddo
+
+   rebin_exp=0
+   do while ( (j .lt. 5) .and. Numbins/2**rebin_exp .gt. 5) 
+      j=0
+      rebin_exp=rebin_exp+1
+      call REBIN(Valu_SM,Valu_SM_rebin,2**rebin_exp)
+      call REBIN(Valu_BSM,Valu_BSM_rebin,2**rebin_exp)
+      do i = 1,NumBins/2**rebin_exp
+         if (Valu_SM_rebin(i) .ge. 5d0 .and. Valu_BSM_rebin(i) .ge. 5d0) then
+            j=j+1
+            Events_SM(j) =floor(Valu_SM_rebin(i))
+            Events_BSM(j)=floor(Valu_BSM_rebin(i))
+            sum_Events_SM=sum_Events_SM+Events_SM(j)
+            fbar_sq = fbar_sq + 1.d0*Events_BSM(j)**2/Events_SM(j)
+         endif
+      enddo
+
+   enddo
+   if (j .le. 4) then
+      print *, 'NOT EN0UGH EVENTS FOR MEANINGFUL ANALYSIS'
+      stop
+   endif
+
    NumEventBins=j
    fbar_sq=fbar_sq/sum_Events_SM
 
@@ -135,7 +173,6 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
 !!!
    do j=1,NumEventBins
       chisq=chisq + (Events_BSM(j)*1.0d0 - f*Events_SM(j))**2/(f*Events_SM(j))
-      print *, Events_BSM(j),Events_SM(j)
    enddo
    
 !! RR UNSURE OF THIS
@@ -145,7 +182,7 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
    print *, 'chisq = ', chisq
    if (mod(NumEventBins,2).ne.0) then
       print *, 'WARNING: ODD NUMBER OF BINS'
-      print *, 'The expression for the P-value is no longer correct'
+      print *, 'The expression for the P-val is no longer correct'
    endif
    ! now that I have chi-sq, I can get p-value
    P=0d0
@@ -153,7 +190,7 @@ character(len=*),parameter :: fmt1 = "(I2,A,2X,1PE10.3,A,2X,1PE23.16,A,2X,1PE23.
       P=P+(chisq/2d0)**k/factorial(k)*exp(-chisq/2d0)
    enddo
    print *, 'P-value: ', P
-   
+   write(cache,*) Delf1V,Delf1A,P
 
 
 
@@ -170,6 +207,38 @@ contains
     end do
     return
   END FUNCTION factorial
+
+  SUBROUTINE REBIN(distr_old,distr_new,div)
+    implicit none
+    real(8),intent(in) ::  distr_old(:)
+    integer, intent(in) :: div
+    real(8),intent(out) :: distr_new(1:200)
+    integer N,Npr,i,j
+! distr with N bins, distr_new has Npr=N/div bins, div >= 1
+    
+    N = size(distr_old,dim=1)
+!    allocate(distr_new(N))
+!    if ( mod(N,div) .ne. 0 ) then
+!       print *, 'Non integral number of bins!'
+!       stop
+!    endif
+
+    if ( div .lt. 1 ) then
+       print *, 'Final argument should be >= 1'
+       stop
+    endif
+
+    Npr=N/div
+    distr_new = 0d0
+
+    do i = 1,Npr
+       do j = 1,div
+          distr_new(i) = distr_new(i) + distr_old(div*(i-1)+j)
+       enddo
+    enddo
+    
+  end SUBROUTINE REBIN
+    
 
  end program BaurAnalysis
 
